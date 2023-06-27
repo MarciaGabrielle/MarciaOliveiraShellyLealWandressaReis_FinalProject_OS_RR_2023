316454958cd0fc099c614da4473230b4630c49d4
Marcia Oliveira <marciagaby.oliveira@gmail.com>
2023-06-27 19:16:41 -0400

    Create Page and read file

--- SimuladorDeMemoriaVirtual/mem_virtual.c
+++ SimuladorDeMemoriaVirtual/mem_virtual.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+typedef struct Page{
+	char address[9];
+	struct Page *next;
+} Page;
+
+FILE *file;
+Page *first, *last;
+char *alg, *filePath, line[20], tmpAddress[9];
+int pageSize, memSize, numPages;
+int operations = 0, reads = 0, writes = 0, hits = 0, misses = 0, faults = 0, usedPages = 0;
+
 int main(){
 
     return 0;
