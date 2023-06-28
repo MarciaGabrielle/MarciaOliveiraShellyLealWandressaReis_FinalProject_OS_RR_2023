6f3cbde5293fad6ae816e45e26c605a3a73f60b7
Shelly Leal <94469543+ShellyLeal05@users.noreply.github.com>
2023-06-27 20:24:56 -0400

    Update mem_virtual.c

--- SimuladorDeMemoriaVirtual/mem_virtual.c
+++ SimuladorDeMemoriaVirtual/mem_virtual.c
@@ -146,3 +146,102 @@ void Random(char value[9]){
 
 	strcpy(tmp->address, value); // Substitui o endereço da página encontrada pelo novo endereço
 }
+
+// Implementa o algoritmo LRU (Least Recently Used)
+void LRU(char value[9])
+{
+	AddNewPage(value); // Adiciona uma nova página
+
+	if (usedPages == numPages)
+		first = first->next; // Se todas as páginas estiverem sendo usadas, remove a página mais antiga (a primeira)
+}
+
+// Implementa o algoritmo FIFO (First-In-First-Out)
+void FIFO(char value[9])
+{
+	AddNewPage(value); // Adiciona uma nova página
+
+	if (usedPages == numPages)
+		first = first->next; // Se todas as páginas estiverem sendo usadas, remove a página mais antiga (a primeira)
+}
+
+// Procura por um endereço na lista de páginas
+bool Find(char value[9])
+{
+	Page *tmp = first, *prev = NULL;
+
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->address, value) == 0) // Compara o endereço atual com o endereço procurado
+		{
+			if (strcmp(alg, "lru") == 0)
+			{
+				if (prev != NULL)
+				{
+					if (tmp->next != NULL)
+						prev->next = tmp->next; // Remove a página encontrada da lista
+				}
+				else
+				{
+					first = first->next; // Se a página encontrada for a primeira, atualiza o ponteiro "first"
+				}
+
+				last->next = tmp; // Move a página encontrada para o final da lista
+				last = tmp; // Atualiza o ponteiro "last" para a nova última página
+				tmp->next = NULL; // Define o próximo ponteiro da página encontrada como NULL
+			}
+
+			return true; // Retorna true se o endereço for encontrado
+		}
+
+		prev = tmp; // Atualiza o ponteiro "prev" para o nó atual
+		tmp = tmp->next; // Atualiza o ponteiro "tmp" para o próximo nó
+	}
+
+	return false; // Retorna false se o endereço não for encontrado
+}
+
+// Substitui uma página na memória de acordo com o algoritmo especificado
+void ReplacePage(char value[9])
+{
+	if (strcmp(alg, "lru") == 0)
+	{
+		LRU(value);
+	}
+	else if (strcmp(alg, "random") == 0)
+	{
+		Random(value);
+	}
+	else if (strcmp(alg, "fifo") == 0)
+	{
+		FIFO(value);
+	}
+}
+
+// Realiza uma operação de escrita no endereço especificado
+void WriteAddress(char value[9])
+{
+	if (usedPages < numPages)
+	{
+		AddNewPage(tmpAddress); // Se ainda houver espaço disponível na memória, adiciona uma nova página
+	}
+	else
+	{
+		faults++; // Contabiliza uma falha (page fault)
+		ReplacePage(tmpAddress); // Substitui uma página de acordo com o algoritmo especificado
+	}
+}
+
+// Libera a memória alocada para as páginas
+void FreeMemory()
+{
+	Page *tmp = first;
+	Page *tmp2;
+
+	while (tmp != NULL)
+	{
+		tmp2 = tmp;
+		tmp = tmp->next;
+		free(tmp2); // Libera a memória alocada para cada página
+	}
+}
